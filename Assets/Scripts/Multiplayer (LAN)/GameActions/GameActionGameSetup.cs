a88b5635ce2ae5ab5d4f05c460772ee1a08e86d1
Markus Koffman <markus.koffman@ownit.nu>
2022-11-08 14:32:30 +0100

    Setupgrejer

--- Assets/Scripts/Multiplayer (LAN)/GameActions/GameActionGameSetup.cs
+++ Assets/Scripts/Multiplayer (LAN)/GameActions/GameActionGameSetup.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameActionGameSetup : GameAction
 {	
 	public bool reciprocate = false;
+	public List<string> opponentChampions = new List<string>();
 	public GameActionGameSetup()
 	{
 		Type = 14;
