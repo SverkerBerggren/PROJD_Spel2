54f18c5bcf3dd79d4d9e99514298cfc8604005ad
MarcusHedenberg <mackanhed@gmail.com>
2022-11-09 10:43:58 +0100

    Update CardTargeting.cs

--- Assets/Scripts/Card/CardTargeting.cs
+++ Assets/Scripts/Card/CardTargeting.cs
@@ -42,7 +42,7 @@ public class CardTargeting : MonoBehaviour
 
         if (gameState.isOnline)
         {
-            if (!gameState.isItMyTurn && !gameState.hasPriority)
+            if (!gameState.isItMyTurn || !gameState.hasPriority)
             {
                 CardGoBackToStartingPosition();
                 return;
