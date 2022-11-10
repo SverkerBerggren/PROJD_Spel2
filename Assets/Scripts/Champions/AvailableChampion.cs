fdb4ace3fbe37d8b6c826429ed6709232ed3817f
Markus Koffman <markus.koffman@ownit.nu>
2022-11-10 18:54:30 +0100

    shank nerf

--- Assets/Scripts/Champions/AvailableChampion.cs
+++ Assets/Scripts/Champions/AvailableChampion.cs
@@ -144,7 +144,6 @@ public class AvailableChampion : MonoBehaviour
         if (champion.destroyShield)
         {
             //armorEffect.DamageArmor(10);
-            print("RUns");
         }
             
 		UpdateTextOnCard();
