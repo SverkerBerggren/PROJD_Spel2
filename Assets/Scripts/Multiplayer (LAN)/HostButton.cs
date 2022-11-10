52cdf8a366d9cc804be43593a5f99def5c311b4c
MarcusHedenberg <mackanhed@gmail.com>
2022-11-10 17:24:29 +0100

    das

--- Assets/Scripts/Multiplayer (LAN)/HostButton.cs
+++ Assets/Scripts/Multiplayer (LAN)/HostButton.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
 using UnityEngine;
-
+using TMPro;
 public class HostButton : MonoBehaviour
 {
     // public Server server = new Server(); 
     ClientConnection clientConnection;
+    public TMP_Text ipAdressText;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +20,29 @@ public class HostButton : MonoBehaviour
     {
         
     }
+    public static string LocalIPAddress()
+    {
+        IPHostEntry host;
+        string localIP = "0.0.0.0";
+        host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (IPAddress ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                localIP = ip.ToString();
+                break;
+            }
+        }
+        return localIP;
+    }
 
     public void HostServer()
     {
 
         print("Hostar den flera gangar");
         FindObjectOfType<ServerHolder>().StartServer();
+        ipAdressText.gameObject.SetActive(true);
+        ipAdressText.text = LocalIPAddress();
 
         clientConnection.isHost = true; 
     }
