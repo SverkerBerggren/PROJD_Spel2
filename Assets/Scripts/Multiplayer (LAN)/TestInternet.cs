7c0bacd6460932fc974ec8fb0ba45a526d62b99c
SverkerBerggren <82815654+SverkerBerggren@users.noreply.github.com>
2022-11-14 12:10:42 +0100

    Update TestInternet.cs

--- Assets/Scripts/Multiplayer (LAN)/TestInternet.cs
+++ Assets/Scripts/Multiplayer (LAN)/TestInternet.cs
@@ -93,7 +93,7 @@ public class TestInternet : MonoBehaviour
                 {
                     Graveyard.Instance.AddCardToGraveyardOpponent(register.cardRegister[card]);
                     ActionOfPlayer actionOfPlayer = ActionOfPlayer.Instance;
-                    actionOfPlayer.ChangeCardOrder(false, actionOfPlayer.handOpponent.cardsInHand[0].GetComponent<CardDisplay>();
+                    actionOfPlayer.ChangeCardOrder(false, actionOfPlayer.handOpponent.cardsInHand[0].GetComponent<CardDisplay>());
                 }
 
             }
