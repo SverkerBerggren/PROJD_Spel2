d95a020a6aa85f740ac9881d941c634ed980e837
fionaliu66 <fiona.liu0606@gmail.com>
2022-11-17 22:50:41 +0100

    Slash

--- Assets/Scripts/VFX/TestScript.cs
+++ Assets/Scripts/VFX/TestScript.cs
@@ -5,21 +5,47 @@ using UnityEngine;
 public class TestScript : MonoBehaviour
 {
     [SerializeField] private GameObject championToProtect;
-  
-    // Start is called before the first frame update
-    void OnGUI()
-    {
-        if (GUI.Button(new Rect(Screen.width / 2 - 50, 5, 100, 30), "Test"))
-        {
-            EffectController.Instance.ActiveShield(championToProtect, 10);
-        }
-    }
+    [SerializeField] private Animator builderAnim;
+    [SerializeField] private GameObject slash;
+    [SerializeField] private float slashDelay;
 
+    // Start is called before the first frame update
+    //void OnGUI()
+    //{
+    //    if (GUI.Button(new Rect(Screen.width / 2 - 50, 5, 100, 30), "Test"))
+    //    {
+    //        EffectController.Instance.ActiveShield(championToProtect, 10);
+    //    }
+    //}
+    private void Start()
+    {
+
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            EffectController.Instance.DestoryShield(championToProtect);
+            //EffectController.Instance.DestoryShield(championToProtect);
+            //slash.SetActive(true);
+            builderAnim.SetTrigger("Attack");
+            StartCoroutine(SlashAttack());
+            
         }
     }
+
+    IEnumerator SlashAttack()
+    {
+        yield return new WaitForSeconds(slashDelay);
+        slash.SetActive(true);
+        yield return new WaitForSeconds(1);
+        DIsableSlash();
+
+
+    }
+
+    void DIsableSlash()
+    {
+        slash.SetActive(false);
+    }
+    
 }
