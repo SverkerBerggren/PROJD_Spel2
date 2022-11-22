2cea1fc498c4bc97a8821b82b137ed1a9d26e6e8
Markus Koffman <markus.koffman@ownit.nu>
2022-11-22 11:12:57 +0100

    Parserupdate

---
+++ Assets/Scripts/Card/CardParser.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class CardParser : MonoBehaviour
+{
+    [SerializeField] private string[] keywords;
+    public Dictionary<string, Func<CardDisplay, string>> actions = new Dictionary<string, Func<CardDisplay, string>>();
+
+    private static CardParser instance;
+    public static CardParser Instance { get; set; }
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        foreach (string keyword in keywords)
+        {
+            Func<CardDisplay, string> action = null;
+            switch (keyword)
+            {
+                case "*Attack*":
+                action = Attack;
+                break;
+
+                case "*Heal*":
+                action = Heal;
+                break;
+
+                case "*Shield*":
+                action = Shield;
+                break;
+
+                case "*Draw*":
+                action = Draw;
+                break;
+
+                case "*Discard*":
+                action = Discard;
+                break;
+            }
+            actions.Add(keyword, action);
+        }
+    }
+
+    public void CheckKeyword(CardDisplay card)
+    {
+        string[] s = card.description.text.Split(" ");
+        string returnString = "";
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (actions.Keys.Contains(s[i]))
+            {
+                if ((s[i] == "*Draw*" || s[i] == "*Discard*") && Convert.ToInt32(s[i + 1]) > 1)
+                    s[i + 1] = "cards";
+                
+                s[i] = actions[s[i]](card);
+            }
+            returnString += s[i];
+            if(i + 1 != s.Length)
+                returnString += " ";
+        }
+        card.description.text = returnString;
+    }
+
+    public string Attack(CardDisplay cardDisplay)
+    {
+        AttackSpell card = (AttackSpell)cardDisplay.card;
+        return (card.damage).ToString();
+    }
+
+    public string Shield(CardDisplay cardDisplay)
+    {
+        HealAndShieldChampion card = (HealAndShieldChampion)cardDisplay.card;
+        return (card.amountToDefence).ToString();
+    }
+
+    public string Heal(CardDisplay cardDisplay)
+    {
+        HealAndShieldChampion card = (HealAndShieldChampion)cardDisplay.card;
+        return (card.amountToHeal).ToString();
+    }
+
+    public string Draw(CardDisplay cardDisplay)
+    {
+        Card card = cardDisplay.card;
+        if (card.amountOfCardsToDraw == 1)
+        {
+            return "a";
+        }
+        return (card.amountOfCardsToDraw).ToString();
+    }
+
+    public string Discard(CardDisplay cardDisplay)
+    {
+        Card card = cardDisplay.card;
+        if (card.amountOfCardsToDiscard == 1)
+        {
+            return "a";
+        }
+        return (card.amountOfCardsToDiscard).ToString();
+    }
+}
