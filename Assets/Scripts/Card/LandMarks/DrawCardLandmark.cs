4e602d552d0d52a35ac87c0c632ad62e4551d72c
Markus Koffman <markus.koffman@ownit.nu>
2022-11-23 13:50:45 +0100

    Update DrawCardLandmark.cs

--- Assets/Scripts/Card/LandMarks/DrawCardLandmark.cs
+++ Assets/Scripts/Card/LandMarks/DrawCardLandmark.cs
@@ -6,10 +6,7 @@ using UnityEngine;
 public class DrawCardLandmark : Landmarks
 {
     public bool mysteriousForest = false;
-    public DrawCardLandmark(DrawCardLandmark card) : base(card.minionHealth, card.cardName, card.description, card.artwork, card.maxManaCost, card.tag, card.damage, card.amountToHeal, card.amountToShield)
-    {
-
-    }
+    public DrawCardLandmark(DrawCardLandmark card) : base(card.minionHealth, card.cardName, card.description, card.artwork, card.maxManaCost, card.tag, card.damage, card.amountToHeal, card.amountToShield) {}
 
     public override void UpKeep()
     {
@@ -17,12 +14,12 @@ public class DrawCardLandmark : Landmarks
         GameState.Instance.DrawCard(1, null);
         if (mysteriousForest && GameState.Instance.amountOfTurns >= 10)
         {
-			GameState.Instance.DrawCard(4, null);
+			GameState.Instance.DrawCard(5, null);
             foreach (LandmarkDisplay l in GameState.Instance.playerLandmarks)
             {
                 if (l.card is DrawCardLandmark && (DrawCardLandmark)l.card)
                 {
-                    
+                    l.DestroyLandmark();
                 }
 
             }
