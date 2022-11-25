84be7285719d821cc03de3d7e738ad3a43215d2e
SverkerBerggren <svartaspjutet@gmail.com>
2022-11-25 21:34:06 +0100

    dsa

--- Assets/ConnectOnline.cs
+++ Assets/ConnectOnline.cs
@@ -1,11 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class ConnectOnline : MonoBehaviour
 {
+    public EnalbeUIButton button;
+    public bool enableNewRoom = false;
     public void OnClick()
     {
+        Thread connectToServer = new Thread(ConnectToServer);
+
+        connectToServer.Start();
+
+      //  button.ClickEnableObjects();
+
+    }
+
+    private void Update()
+    {
+        if(enableNewRoom)
+        {
+            button.ClickEnableObjects();
+        }
+    }
+
+    public void ConnectToServer()
+    {
+        
+        Debug.Log("hej");
         ClientConnection.Instance.ConnectToServer("mrboboget.se", 63000);
+        enableNewRoom = true;
+        Debug.Log("hej igen ");
     }
 }
