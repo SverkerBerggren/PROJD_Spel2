5cfe9010cac7671e0168fe9e9c28536062852e2d
MarcusHedenberg <82215349+MarcusHedenberg@users.noreply.github.com>
2022-12-02 14:42:59 +0100

    Update AvailableChampion.cs

--- Assets/Scripts/Champions/AvailableChampion.cs
+++ Assets/Scripts/Champions/AvailableChampion.cs
@@ -83,7 +83,7 @@ public class AvailableChampion : MonoBehaviour
 
         GetAllMeshes();
 
-        if ((gameState.playerChampion == this || gameState.opponentChampion == this) && !gameState.isOnline)
+        if ((gameState.playerChampion == this || gameState.opponentChampion == this))
             SetupHealthbar();
     }
 
@@ -174,16 +174,11 @@ public class AvailableChampion : MonoBehaviour
         champion.UpdatePassive();
         if (passiveEffect.text != null)
             passiveEffect.text = champion.passiveEffect;
-        if (!gameState.isOnline)
-        {
-            healthBarSlider.maxValue = maxHealth;
-            healthBarSlider.value = health;
-            healthBarText.text = health.ToString() + "/" + maxHealth.ToString();
-        }
 
-        healthText.text = health.ToString() + "/" + maxHealth.ToString();
-        
-
+        healthBarSlider.maxValue = maxHealth;
+        healthBarSlider.value = health;
+        healthBarText.text = health.ToString() + "/" + maxHealth.ToString();
+    
         if (shieldText != null)
         {
             if (shield > 0)
