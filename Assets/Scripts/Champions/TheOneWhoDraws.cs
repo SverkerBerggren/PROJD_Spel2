1a7b61adaa7846ac87af7fcc5e366929ce173e0a
Mibot104 <95620601+Mibot104@users.noreply.github.com>
2022-12-09 12:38:18 +0100

    Update TheOneWhoDraws.cs

--- Assets/Scripts/Champions/TheOneWhoDraws.cs
+++ Assets/Scripts/Champions/TheOneWhoDraws.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TheOneWhoDraws", menuName = "Champion/TheOneWhoDraws", order = 1)]
 public class TheOneWhoDraws : Champion
 {
-	int cardsDraw = 1;
+	public int cardsDraw = 1;
     public override void EndStep()
 	{
 		base.EndStep();
-		gameState.DrawCard(1, null);
+		gameState.DrawCard(cardsDraw, null);
 	}
 }
