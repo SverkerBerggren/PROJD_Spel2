6b89f19ba255be38e7df98638d2715c35d7bba37
MarcusHedenberg <82215349+MarcusHedenberg@users.noreply.github.com>
2023-01-15 19:56:54 +0100

    ad

--- Assets/Scripts/UI/Deckbuilder/DeckClicked.cs
+++ Assets/Scripts/UI/Deckbuilder/DeckClicked.cs
@@ -6,8 +6,11 @@ public class DeckClicked : MonoBehaviour
 {
     public void OnClick()
     {
-        ListEnum lE = new ListEnum();
-        lE.myDeck = true;
-        Choice.Instance.ChoiceMenu(lE, 2, WhichMethod.ShowDeck, null);
+        if (GameState.Instance.IsItMyTurn && GameState.Instance.HasPriority)
+        {
+            ListEnum lE = new ListEnum();
+            lE.myDeck = true;
+            Choice.Instance.ChoiceMenu(lE, 2, WhichMethod.ShowDeck, null);
+        }
     }
 }
